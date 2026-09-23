45e8b57cab886fbac48b03f53629993f6bfe7202
MoZacks04 <hoggvalleyzack@gmail.com>
2026-09-23 19:15:34 -0400

    Add separate team status HUD region

--- src/overwatch_vision/regions.py
+++ src/overwatch_vision/regions.py
@@ -14,9 +14,8 @@ class HUDRegionManager:
     def __init__(self, config):
         self.config = config
 
-    def killfeed_search_region(self, game_image):
+    def _crop_from_config(self, game_image, cfg):
         h, w = game_image.shape[:2]
-        cfg = self.config["killfeed"]["search_region"]
 
         rect = normalized_rect_to_pixels(
             width=w,
@@ -27,5 +26,20 @@ class HUDRegionManager:
             bottom=float(cfg["bottom"]),
         )
 
-        crop = game_image[rect.y1:rect.y2, rect.x1:rect.x2]
+        crop = game_image[
+            rect.y1:rect.y2,
+            rect.x1:rect.x2,
+        ]
         return RegionCrop(image=crop, rect=rect)
+
+    def killfeed_search_region(self, game_image):
+        return self._crop_from_config(
+            game_image,
+            self.config["killfeed"]["search_region"],
+        )
+
+    def team_status_region(self, game_image):
+        return self._crop_from_config(
+            game_image,
+            self.config["team_status"]["search_region"],
+        )
